59078332eee470bae3de95b5feeb5fe836f04d9d
SanCheetos <xsancheetosx@gmail.com>
2022-03-12 16:21:07 +0500

    Добавил линии Третьяк

--- List.Designer.cs
+++ List.Designer.cs
@@ -85,6 +85,12 @@ namespace RTiPPO
             this.DoFilter = new System.Windows.Forms.Button();
             this.ThrowOffFilter = new System.Windows.Forms.Button();
             this.Add = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.CaptMinDogs)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.CaptMaxSum)).BeginInit();
@@ -424,7 +430,7 @@ namespace RTiPPO
             // DateMaxCapt
             // 
             this.DateMaxCapt.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.DateMaxCapt.Location = new System.Drawing.Point(1061, 97);
+            this.DateMaxCapt.Location = new System.Drawing.Point(1068, 97);
             this.DateMaxCapt.Name = "DateMaxCapt";
             this.DateMaxCapt.Size = new System.Drawing.Size(98, 23);
             this.DateMaxCapt.TabIndex = 30;
@@ -432,7 +438,7 @@ namespace RTiPPO
             // DateMinCapt
             // 
             this.DateMinCapt.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.DateMinCapt.Location = new System.Drawing.Point(1061, 62);
+            this.DateMinCapt.Location = new System.Drawing.Point(1068, 62);
             this.DateMinCapt.Name = "DateMinCapt";
             this.DateMinCapt.Size = new System.Drawing.Size(98, 23);
             this.DateMinCapt.TabIndex = 29;
@@ -440,7 +446,7 @@ namespace RTiPPO
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(1034, 103);
+            this.label3.Location = new System.Drawing.Point(1041, 103);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(22, 15);
             this.label3.TabIndex = 28;
@@ -449,7 +455,7 @@ namespace RTiPPO
             // label21
             // 
             this.label21.AutoSize = true;
-            this.label21.Location = new System.Drawing.Point(1034, 68);
+            this.label21.Location = new System.Drawing.Point(1041, 68);
             this.label21.Name = "label21";
             this.label21.Size = new System.Drawing.Size(21, 15);
             this.label21.TabIndex = 27;
@@ -507,7 +513,7 @@ namespace RTiPPO
             // CheckDateCapt
             // 
             this.CheckDateCapt.AutoSize = true;
-            this.CheckDateCapt.Location = new System.Drawing.Point(1051, 33);
+            this.CheckDateCapt.Location = new System.Drawing.Point(1058, 33);
             this.CheckDateCapt.Name = "CheckDateCapt";
             this.CheckDateCapt.Size = new System.Drawing.Size(92, 19);
             this.CheckDateCapt.TabIndex = 38;
@@ -574,11 +580,88 @@ namespace RTiPPO
             this.Add.UseVisualStyleBackColor = true;
             this.Add.Click += new System.EventHandler(this.Add_Click);
             // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(130, 36);
+            this.button1.MaximumSize = new System.Drawing.Size(1, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(1, 103);
+            this.button1.TabIndex = 46;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Location = new System.Drawing.Point(295, 36);
+            this.button2.MaximumSize = new System.Drawing.Size(1, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(1, 103);
+            this.button2.TabIndex = 47;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(430, 36);
+            this.button3.MaximumSize = new System.Drawing.Size(1, 0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(1, 103);
+            this.button3.TabIndex = 48;
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(626, 36);
+            this.button4.MaximumSize = new System.Drawing.Size(1, 0);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(1, 103);
+            this.button4.TabIndex = 49;
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.ForeColor = System.Drawing.Color.White;
+            this.button5.Location = new System.Drawing.Point(821, 36);
+            this.button5.MaximumSize = new System.Drawing.Size(1, 0);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(1, 103);
+            this.button5.TabIndex = 50;
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button6.FlatAppearance.BorderSize = 0;
+            this.button6.ForeColor = System.Drawing.Color.White;
+            this.button6.Location = new System.Drawing.Point(1035, 36);
+            this.button6.MaximumSize = new System.Drawing.Size(1, 0);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(1, 103);
+            this.button6.TabIndex = 51;
+            this.button6.UseVisualStyleBackColor = false;
+            // 
             // List
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1244, 675);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.Add);
             this.Controls.Add(this.ThrowOffFilter);
             this.Controls.Add(this.DoFilter);
@@ -697,6 +780,12 @@ namespace RTiPPO
         private System.Windows.Forms.Button DoFilter;
         private System.Windows.Forms.Button ThrowOffFilter;
         private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
     }
 }
 
