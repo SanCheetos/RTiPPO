d6407c3c3b18720268c06cb8a5a7ccaf0e6cfbc3
Retard-me <renarddapinder@gmail.com>
2022-03-20 14:13:45 +0500

    bug fix

--- List.Designer.cs
+++ List.Designer.cs
@@ -767,7 +767,7 @@ namespace RTiPPO
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1244, 995);
+            this.ClientSize = new System.Drawing.Size(1245, 749);
             this.Controls.Add(this.button9);
             this.Controls.Add(this.button8);
             this.Controls.Add(this.button7);
